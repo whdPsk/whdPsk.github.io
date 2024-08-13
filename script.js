6c16ba4ed39e8a230d8749b7cc932d64e91a18d6
whdPsk <165573595+whdPsk@users.noreply.github.com>
2024-08-13 20:31:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,36 @@ async function loadData() {
     return data;
 }
 
+//예상 검색어 보여주기
+async function showSuggestions() {
+    const serachInput = document.getElementById('searchInput').value.trim().toLowerCase();
+    const suggestionsDiv = document.getElementById('suggestions');
+
+    if (searchInput.length === 0) {
+        suggestionsDiv.style.display = 'none';
+        return;
+    }
+
+    const suggestions = await loadSuggestions();
+    const filteredSuggestions = suggestions.filter(term => term.toLowerCase().includes(searchInput));
+
+    if (filteredSuggestions.length > 0) {
+        suggetsionsDiv.intterHTML = '';
+        filteredSuggestions.forEach(suggestion => {
+            const div = document.createElement('div');
+            div.textContent = suggestion;
+            div.onclick = function() {
+                document.getElementById('searchInput').value = suggestion;
+                suggestionsDiv.style.display = 'none';
+            };
+            suggestionsDiv.appendChild(div);
+        });
+        suggestionsDiv.style.display = 'block';
+    } else {
+        suggestionsDiv.style.display = 'none';
+    }
+}
+
 // 검색 함수
 async function searchTerm() {
     const searchInput = document.getElementById('searchInput').value.trim().toLowerCase();
